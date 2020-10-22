fd000e630bfb328cffdab75430f057d503fa428a
Nass1m <70798807+Nass1m@users.noreply.github.com>
2020-10-22 21:55:59 +0200

    voorlopige sitemap

--- contact_page.js
+++ contact_page.js
@@ -3,11 +3,11 @@ function show_alert()
   alert("Form submitted");
 }
 
+
 /*
 function emailCheck() 
 {
-  document.querySelector('#required');
-
+  document.querySelector('#required')
 }*/
 
 //[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$

241d28f05414983ebf45e7415d77729bb548c5ef
ExploHash <jimmy@levarne.nl>
2020-10-27 20:04:33 +0100

    Combined scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//Scroll menu
 window.addEventListener("scroll", updateMenu)
 
 var schrinked = false;
@@ -28,7 +29,31 @@ function initMenu(){
       menu.style.display = "none";
     }
   });
+}
 
+window.addEventListener("load", initOurCheese)
+
+function initOurCheese(){
+    const cheeseParts = Array.from(document.querySelectorAll('.our_cheese--cheese--part'));
+    const cheeseSlide = Array.from(document.querySelectorAll('.our_cheese--slide'));
+
+    for(let cheeseIndex in cheeseParts){
+        let cheesePart = cheeseParts[cheeseIndex];
+
+        cheesePart.addEventListener('click', function(){
+            resetOurCheese();
+            cheeseSlide[cheeseIndex].classList.add('current_slide');
+        });
+    }
+}
+
+function resetOurCheese(){
+    const cheeseParts = Array.from(document.querySelectorAll('.our_cheese--cheese--part'));
+    const cheeseSlide = Array.from(document.querySelectorAll('.our_cheese--slide'));
+
+    for(let cheeseIndex in cheeseParts){
+        cheeseSlide[cheeseIndex].classList.remove('current_slide');
+    }
 }
 
 function mailFunction() {
@@ -40,3 +65,14 @@ function show_alert()
   alert("Form submitted");
 }
 
+function checkEmail(mail){
+  var regex = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
+  if (regex.test(mail.email.value)){
+    alert("Form has been submitted")
+    return true;
+  }
+  else{
+    alert("Please fill in a valid E-mail adress")
+    return false;
+  }
+}
